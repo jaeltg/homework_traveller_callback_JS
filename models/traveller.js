3c8c87cc292998311408d89b37333a3315ccba6b
jaeltg <jaeltg@hotmail.com>
2021-03-31 17:38:50 +0100

    inital commit - MVP and extensions finished

---
+++ models/traveller.js
@@ -0,0 +1,46 @@
+const Traveller = function(journeys) {
+  this.journeys = journeys;
+};
+
+Traveller.prototype.getJourneyStartLocations = function() {
+  return this.journeys.map((journey) => {
+    return journey.startLocation
+  })
+};
+
+Traveller.prototype.getJourneyEndLocations = function () {
+  return this.journeys.map((journey) => {
+    return journey.endLocation
+  })
+};
+
+Traveller.prototype.getJourneysByTransport = function (transport) {
+  return this.journeys.filter((journey) => {
+    return journey.transport === transport
+  })
+};
+
+Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
+  return this.journeys.filter((journey) => {
+    return journey.distance >= minDistance
+  })
+};
+
+Traveller.prototype.calculateTotalDistanceTravelled = function () {
+  return this.journeys.reduce((totalDistance, journey) => {
+    return totalDistance + journey.distance
+  }, 0)
+};
+
+Traveller.prototype.getUniqueModesOfTransport = function () {
+  let modesOfTransport = [];
+  this.journeys.forEach((journey) => {
+    if (!modesOfTransport.includes(journey.transport)) {
+      modesOfTransport.push(journey.transport)
+    } 
+  })
+  return modesOfTransport
+};
+
+
+module.exports = Traveller;
